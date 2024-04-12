34857ce50a0aaa054df18d2090d0a5c82e934af4
Hyojin Lim <hyoin614@naver.com>
2024-04-12 10:19:24 +0900

    #9 sizeof 연산자로 본 각 자료형의 크기

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -31,7 +31,13 @@ namespace CSBasic
             Console.WriteLine(double.MinValue);
             Console.WriteLine(double.MaxValue);
 
-          
+            //sizeof 연산자
+            Console.WriteLine("int :" + sizeof(int));
+            Console.WriteLine("long : " + sizeof(long));
+            Console.WriteLine("double : "+sizeof(double));
+            Console.WriteLine("float : " + sizeof(float));
+            Console.WriteLine("char : " + sizeof(char));
+            Console.WriteLine("bool : " + sizeof(bool));
         }
     }
 }
